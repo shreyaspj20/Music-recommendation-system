8455021219974d2707ea0f9dad1a9f4a36eb7db1
SHREYAS PJ <shreyaspj20@gmail.com>
2020-12-13 12:45:31 +0530

    adding new features

--- emotion_video_classifier.py
+++ emotion_video_classifier.py
@@ -1,112 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
-from keras.preprocessing.image import img_to_array
-import imutils
-import cv2
 from keras.models import load_model
 import numpy as np
+import cv2
+from keras.preprocessing import image
 
-
-# In[2]:
-
-
-detection_model_path = 'haarcascade_frontalface_default.xml'
-emotion_model_path = 'final_model1.h5'
-
-
-# In[3]:
-
-
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'final_model.h5'
 face_detection = cv2.CascadeClassifier(detection_model_path)
 emotion_classifier = load_model(emotion_model_path, compile=False)
-EMOTIONS = ["happy","sad","neutral"]
+EMOTIONS = ["happy", "sad"]
 
 
-# In[ ]:
+def emotion_testing():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, test_img = cap.read()
+        if not ret:
+            continue
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
+        faces_detected = face_detection.detectMultiScale(gray_img, 1.32, 5)
 
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+            roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+            roi_gray = cv2.resize(roi_gray, (48, 48))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_pixels /= 255
 
-cv2.namedWindow('your_face')
-camera = cv2.VideoCapture(0)
-while True:
-    frame = camera.read()[1]
-    #reading the frame
-    frame = imutils.resize(frame,width=300)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
-    
-    canvas = np.zeros((250, 300, 3), dtype="uint8")
-    frameClone = frame.copy()
-    if len(faces) > 0:
-        faces = sorted(faces, reverse=True,
-        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-        (fX, fY, fW, fH) = faces
-                    # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
-            # the ROI for classification via the CNN
-        roi = gray[fY:fY + fH, fX:fX + fW]
-        roi = cv2.resize(roi, (48, 48))
-        roi = roi.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = np.expand_dims(roi, axis=0)
-        
-        
-        preds = emotion_classifier.predict(roi)[0]
-        emotion_probability = np.max(preds)
-        label = EMOTIONS[preds.argmax()]
-    else: continue
+            predictions = emotion_classifier.predict(img_pixels)
 
- 
-    for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
-                # construct the label text
-                text = "{}: {:.2f}%".format(emotion, prob * 100)
+            # find max indexed array
+            max_index = np.argmax(predictions[0])
+            predicted_emotion = EMOTIONS[max_index]
 
-                # draw the label + probability bar on the canvas
-               # emoji_face = feelings_faces[np.argmax(preds)]
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-                
-                w = int(prob * 300)
-                cv2.rectangle(canvas, (7, (i * 35) + 5),
-                (w, (i * 35) + 35), (0, 0, 255), -1)
-                cv2.putText(canvas, text, (10, (i * 35) + 23),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45,
-                (255, 255, 255), 2)
-                cv2.putText(frameClone, label, (fX, fY - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-                cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
-                              (0, 0, 255), 2)
-#    for c in range(0, 3):
-#        frame[200:320, 10:130, c] = emoji_face[:, :, c] * \
-#        (emoji_face[:, :, 3] / 255.0) + frame[200:320,
-#        10:130, c] * (1.0 - emoji_face[:, :, 3] / 255.0)
+        resized_img = cv2.resize(test_img, (1000, 700))
+        cv2.imshow('Facial emotion analysis ', resized_img)
 
-
-    cv2.imshow('your_face', frameClone)
-    cv2.imshow("Probabilities", canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-camera.release()
-cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows
+    return predicted_emotion
