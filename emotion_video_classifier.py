10bd1068f9ab06a922be47c9a3fffa84f008757b
SHREYAS PJ <shreyaspj20@gmail.com>
2021-01-10 20:00:31 +0530

    adding GUI

--- emotion_video_classifier.py
+++ emotion_video_classifier.py
@@ -1,4 +1,3 @@
-
 from keras.models import load_model
 import numpy as np
 import cv2
--- run.py
+++ run.py
@@ -1,15 +1,54 @@
+from tkinter import ttk
+
+from PIL import Image, ImageTk
+from PIL.ImageTk import PhotoImage
+from sklearn.metrics import silhouette_score
 import config
 import spotipy
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 
 from emotion_video_classifier import emotion_testing
+import tkinter as tk
+from tkinter import messagebox
 
 client_credentials_manager = SpotifyClientCredentials(client_id=config.cid, client_secret=config.secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-print("Enter name of the artist")
-name = input()  # chosen artist
+root = tk.Tk()
+root.title('CREDENTIALS')
+root.geometry("600x400")
+root.configure(bg='black')
+name1 = tk.StringVar()
+
+photo = PhotoImage(file="musicback.jpg")
+l = tk.Label(root, image=photo)
+l.image = photo  # just keeping a reference
+l.grid()
+
+
+def submit():
+    global name
+    name = name_entry.get()
+    messagebox.showinfo("Information", "Wait for sometime for us to create Playlists")
+    root.destroy()
+
+
+name_label = tk.Label(root, text='Enter Name of Artist',
+                      font=('calibre',
+                            10, 'bold'))
+
+name_entry = tk.Entry(root,
+                      textvariable=name1, font=('calibre', 10, 'normal'))
+
+sub_btn = tk.Button(root, text='Submit',
+                    command=submit)
+
+name_label.grid(row=0, column=0)
+name_entry.grid(row=3, column=0)
+sub_btn.grid(row=5, column=0)
+root.mainloop()
+
 result = sp.search(name)  # search query
 
 artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
@@ -140,18 +179,13 @@ kmeans = KMeans(init="k-means++",
                 n_clusters=2,
                 random_state=15).fit(X)
 data1['kmeans'] = kmeans.labels_
+# print(silhouette_score(X, data1['kmeans'], metric='euclidean'))
 
 data2 = data1[:data.shape[0]]
 cluster = data2.groupby(by=data2['kmeans'])
 data2.pop('kmeans')
 df1 = cluster.apply(lambda x: x.sort_values(["popularity"], ascending=False))
 df1.reset_index(level=0, inplace=True)
-
-emotion_word = emotion_testing()
-if emotion_word == 'sad':
-    emotion_code = 0
-else:
-    emotion_code = 1
 
 
 def get_results(emotion_code):
@@ -166,8 +200,64 @@ def get_results(emotion_code):
         return pd.DataFrame(sad_set).T
 
 
-print(get_results(emotion_code))
+def final():
+    root1 = tk.Tk()
+    root1.title("Your Playlist")
+    root1.configure(bg='black')
+
+    df = get_results(emotion_code)
+    cols = list(df.columns)
+    tree = ttk.Treeview(root1)
+    tree.pack(side=tk.TOP, fill=tk.X)
+    tree["columns"] = cols
+    for k in cols:
+        tree.column(k, anchor="w")
+        tree.heading(k, text=k, anchor='w')
+
+    for index, row in df.iterrows():
+        tree.insert("", 0, text=index, values=list(row))
+
+    root1.mainloop()
+    if emotion_word == 'sad':
+        print('emotion detected is SAD')
+    else:
+        print('emotion detected is HAPPY')
+
+
+emotion_word = (emotion_testing())
 if emotion_word == 'sad':
-    print('emotion detected is SAD')
+    emotion_code = 0
 else:
-    print('emotion detected is HAPPY')
+    emotion_code = 1
+
+window = tk.Tk()
+window.title("Music Recommender System")
+window.configure(background='black')
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+message = tk.Label(
+    window, text="Music Recommender System",
+    bg="yellow", fg="black", width=50,
+    height=3, font=('times', 30, 'bold'))
+
+message.place(x=200, y=20)
+pred = tk.Button(window, text="print",
+                 command=final, fg="white", bg="black",
+                 width=20, height=3, activebackground="Red",
+                 font=('times', 15, ' bold '))
+pred.place(x=200, y=500)
+
+quitWindow = tk.Button(window, text="Quit",
+                       command=window.destroy, fg="white", bg="black",
+                       width=20, height=3, activebackground="Red",
+                       font=('times', 15, ' bold '))
+quitWindow.place(x=1100, y=500)
+
+image1 = Image.open("musicimg (1).jpg")
+test = ImageTk.PhotoImage(image1)
+
+label1 = tk.Label(image=test)
+label1.image = test
+label1.place(x=470, y=150)
+root.mainloop()
+window.mainloop()
